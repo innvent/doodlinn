c21065cdbce4399391fb9810c9af2930f0ec19e5
Rudy Seidinger <rudyseidinger@gmail.com>
2012-11-28 14:06:33 -0200

    Adiciona start app rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doodlinn::Application.config.secret_token = '74929725d3aea50a41bb5cb052f444a9b53867bb5f77fa73ef64fe4eac1c448e61af15baecf6a6942d9af3f0b00422bb717458783ed5022bc3e46d56834b66f6'
