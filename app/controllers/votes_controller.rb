b8a9bc1347980fca750cbb5132ac1a59edffa8c4
Artur Rodrigues <arturhoo@gmail.com>
2012-11-29 10:30:13 -0200

    vote now works, participant's email validation

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,5 +9,18 @@ class VotesController < ApplicationController
     @event = Event.find_by_token params[:event_id]
     # @vote = Vote.new(:event =>@event)
     @vote = @event.votes.new(params[:vote])
+    if params[:dates]
+      dates_array = params[:dates].map{ |x| Date.strptime(x, '%Y-%m-%d') }
+    else
+      dates_array = []
+    end
+    @vote.dates_array = dates_array
+    if @vote.save
+      flash[:notice] = "Voted!"
+      redirect_to token_path(@event)
+    else
+      flash[:error] = "Something went wrong!"
+      render 'new'
+    end
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,11 @@
 class Vote < ActiveRecord::Base
   attr_accessible :dates_array, :participant
   validate :dates_array_contains_only_date_objects
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :participant, presence:   true,
+                          format:     { with: VALID_EMAIL_REGEX },
+                          uniqueness: { case_sensitive: false }
+
   belongs_to :event
   serialize :dates_array, Array
 
@@ -9,8 +14,9 @@ class Vote < ActiveRecord::Base
   end
 
   private
-  def dates_array_contains_only_date_objects
-    elements_found = dates_array.select{|x| !x.instance_of?(Date)}
-    errors[:dates_array] = "Dates array should contain only date objects!" if elements_found.any?
-  end
+
+    def dates_array_contains_only_date_objects
+      elements_found = dates_array.select{|x| !x.instance_of?(Date)}
+      errors[:dates_array] = "Dates array should contain only date objects!" if elements_found.any?
+    end
 end
